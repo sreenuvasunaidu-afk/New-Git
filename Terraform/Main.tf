58000085861f4871786e4ebf2a5f2ee2bb6647c6
sreenuvasunaidu-afk <sreenuvasu.naidu@gmail.com>
2025-11-30 10:34:27 +0530

    Adding the files to git

--- Terraform/Main.tf
+++ Terraform/Main.tf
@@ -1,27 +1,38 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
 resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "mini-project-vpc" }
+  tags = {
+    Name = "mini-project-vpc"
+  }
 }
+
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-south-1a"
-  tags = { Name = "mini-project-public-subnet" }
+  tags = {
+    Name = "mini-project-public-subnet"
+  }
 }
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main_vpc.id
-  tags = { Name = "mini-project-igw" }
+  tags = {
+    Name = "mini-project-igw"
+  }
 }
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main_vpc.id
-  tags = { Name = "mini-project-public-rt" }
+  tags = {
+    Name = "mini-project-public-rt"
+  }
 }
 
 resource "aws_route" "internet_access" {
@@ -34,9 +45,10 @@ resource "aws_route_table_association" "public_subnet_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
+
 resource "aws_security_group" "web_sg" {
   name        = "mini-project-sg"
-  description = "Allow SSH & HTTP"
+  description = "Allow SSH, HTTP & Jenkins"
   vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
@@ -53,6 +65,13 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Jenkins console
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -60,21 +79,44 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = { Name = "mini-project-sg" }
+  tags = {
+    Name = "mini-project-sg"
+  }
 }
-resource "aws_key_pair" "demo_key" {
-  key_name   = "mini-project-key"
-  public_key = file("C:/Users/dpava/.ssh/id_rsa.pub")
+
+# Reference the pre-existing key pair
+data "aws_key_pair" "demo_key" {
+  key_name = "mini-project-key"
 }
+
+# Dynamic lookup for latest Ubuntu 20.04 AMI in ap-south-1
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (official Ubuntu)
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami                    = "ami-0c02fb55956c7d316" # Ubuntu 20.04
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name               = aws_key_pair.demo_key.key_name
+  ami                         = data.aws_ami.ubuntu.id  # Dynamic AMI lookup
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  key_name                    = data.aws_key_pair.demo_key.key_name
   associate_public_ip_address = true
-  tags = { Name = "mini-project-ec2" }
+  tags = {
+    Name = "mini-project-ec2"
+  }
 }
+
 output "ec2_public_ip" {
   description = "Public IP of EC2 instance"
   value       = aws_instance.web.public_ip
@@ -87,9 +129,8 @@ output "vpc_id" {
 output "subnet_id" {
   value = aws_subnet.public_subnet.id
 }
-ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Jenkins console
-  }
+
+output "ubuntu_ami_id" {
+  description = "AMI ID of Ubuntu instance"
+  value       = data.aws_ami.ubuntu.id
+}
